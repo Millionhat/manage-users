be8970bacb93f1c8651edbcc8b110f29f9fcc9fc
Millionhat <38446282+Millionhat@users.noreply.github.com>
2021-04-10 17:28:23 -0500

    creacion proyecto y agregados config de routes y axios

---
+++ src/config/routes.js
@@ -0,0 +1,11 @@
+import {Route, Switch} from "react-router-dom";
+
+import Home from "../Pages/Home";
+import UserList from "../Pages/UserList";
+import NotFound from "../Pages/NotFound";
+import User from "../Pages/User";
+
+export default(
+    <Switch>
+    </Switch>
+)
